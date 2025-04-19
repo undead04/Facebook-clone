2bd47fc939838e3409bab93de90d03374753d652
undead04 <antran.261004@gmail.com>
2025-04-19 15:52:56 +0700

    remove dist

--- dist/src/helpers/asyncHandle.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const asyncHandle = (fn) => {
-    return (req, res, next) => {
-        fn(req, res).catch(next);
-    };
-};
-exports.default = asyncHandle;
--- dist/src/models/cacheContant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VERIFY = exports.USER = void 0;
-const USER = "USER";
-exports.USER = USER;
-const VERIFY = "VERIFY";
-exports.VERIFY = VERIFY;
